779ba45730ad21ee63347e28aab20e3eb34049d6
Pythonesa <pythonesamar@gmail.com>
2025-04-07 12:17:38 -0300

    💄 add menu and order containers

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ function App() {
       <header >
         <h1>Consumina</h1>
       </header>
+
+      <main>
+        <div id="menu">
+          <h2>Menu</h2>
+        </div>
+        <div id="cart">
+          <h2>Orden</h2>
+        </div>
+      </main>
     </>
   )
 }
